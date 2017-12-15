4e12caf0b9b32ef555adaa8fcf82026be9f56938
github username <github email address>
2017-12-14 20:32:19 -0500

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,14 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :song, :name, :artist_name
+
   @@all = []
+
+  def initialize(name="", artist_name="")
+    @name = name
+    @artist_name = artist_name
+  end
 
   def self.all
     @@all
@@ -10,4 +18,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @song = Song.new
+    @song.save
+    @song
+  end
+
+  def self.new_by_name(name)
+    #binding.pry
+    @song = Song.new(name)
+    @song
+  end
+
+  def self.create_by_name(name)
+    @song = Song.new(name)
+    @song.save
+    @song
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    filename_array = filename.split(/ - |\.mp3/)
+    @song = Song.new(filename_array[1], filename_array[0])
+  end
+
+  def self.create_from_filename(filename)
+    filename_array = filename.split(/ - |\.mp3/)
+    @song = Song.new(filename_array[1], filename_array[0])
+    @@all << @song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
 end
+
+
+
+#Pry.start
